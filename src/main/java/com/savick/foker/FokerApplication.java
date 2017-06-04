921ac64de465de49c0165b9b604c484c46f6a782
skaveesh <skaveesh@gmail.com>
2017-06-04 09:22:23 +0530

    Initial commit

---
+++ src/main/java/com/savick/foker/FokerApplication.java
@@ -0,0 +1,12 @@
+package com.savick.foker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FokerApplication.class, args);
+	}
+}
